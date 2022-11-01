327701b584195814b9cda89c269a851d81fad9e6
MathurJi <anileshjjssacademy84@gmail.com>
2022-11-01 17:37:55 +0530

    add the new component

---
+++ src/api/index.js
@@ -0,0 +1,54 @@
+import {API_URLS,LOCALSTORAGE_TOKEN_KEY } from "../utils";
+
+const customFetch =async (url,{body,...customConfig})=>{
+    //second orgument in above is the configration object.
+    const token=window.localStorage.getItem(LOCALSTORAGE_TOKEN_KEY);
+
+    const headers={
+        'content-type':'application/json',
+        Accept:'applocation/json'
+    }
+
+    if(token){
+        headers.Authorization = `Bearer ${token}`;
+    }
+
+    const config={
+        ...customConfig,
+        headers:{
+            ...headers,
+            ...customConfig.headers,
+        }
+    }
+
+    if(body){
+        config.body=JSON.stringify(body)
+    }
+    try{
+        const responce=await fetch(url,config);
+        //config means method
+        const data =await responce.json();
+        if(data.success){
+            return{
+                data:data.data,
+                success:true
+            }
+        }
+        throw new Error(data.message);
+
+        
+    }
+    catch (error){
+        console.error('error');
+        return {
+            message:error.message,
+            success:false,
+        }
+    }
+};
+
+export const getPosts=(page=1,limit=5)=>{
+    return customFetch(API_URLS.posts(page,limit),{
+        method:'GET',
+    });
+} 
--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
-
-
+import { useEffect } from "react";
+import {getPosts} from '../api'
+import {Home} from '../pages';
 function App() {
+  useEffect(()=>{
+    const fetchPosts =async ()=>{
+      const response=await getPosts();
+      console.log('response',response);
+    }
+    fetchPosts();
+  },[]);
   return (
     <div className="App">
-      <h1>Social Media</h1>
+      <Home/>
     </div>
   );
 }
